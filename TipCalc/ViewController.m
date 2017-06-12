1247fb5f01ab08e99785c600002a2ac03bebc98a
Katrina de Guzman <katrinavdeguzman@gmail.com>
2017-06-11 22:24:39 -0700

    Split bill

--- TipCalc/ViewController.m
+++ TipCalc/ViewController.m
@@ -14,6 +14,9 @@
 @property (weak, nonatomic) IBOutlet UILabel *tipLabel;
 @property (weak, nonatomic) IBOutlet UILabel *totalLabel;
 @property float currentVal;
+@property (weak, nonatomic) IBOutlet UILabel *numberOfPeopleLabel;
+@property (weak, nonatomic) IBOutlet UISlider *numberOfPeopleSlider;
+@property int people;
 
 @end
 
@@ -25,24 +28,41 @@
     
     _tipPercentageSlider.minimumValue = 10;
     _tipPercentageSlider.maximumValue = 30;
+    
+    _numberOfPeopleSlider.minimumValue = 2;
+    _numberOfPeopleSlider.maximumValue = 8;
+    _totalLabel.text = @"0.00";
 }
 - (IBAction)tipPercentSlider:(id)sender
 {
     _currentVal = (int)self.tipPercentageSlider.value;
     NSLog(@"%f", self.currentVal);
     
-    NSString* tipString = [NSString stringWithFormat:@"%.f", self.currentVal];
+    NSString* tipString = [NSString stringWithFormat:@"%.f%%", self.currentVal];
     self.tipLabel.text = tipString;
-}
-
-- (IBAction)calculateTipButton:(id)sender
-{
+    
     float total =  [[self.billAmountTextField text] floatValue];
     float totalWithTip = total * (self.currentVal/100) + total ;
-    NSString* totalWithTipString = [NSString stringWithFormat:@"%.2f", totalWithTip];
+    float divideByPeople = totalWithTip/(1*self.people);
+    NSString* totalWithTipString = [NSString stringWithFormat:@"%.2f", divideByPeople];
     self.totalLabel.text = totalWithTipString;
 }
+- (IBAction)calculateTipButton:(id)sender
+{
 
+}
+- (IBAction)numberOfPeopleSlider:(id)sender
+{
+    _people = (int)self.numberOfPeopleSlider.value;
+    NSString* peopleString = [NSString stringWithFormat:@"%i people", self.people];
+    self.numberOfPeopleLabel.text = peopleString;
+    
+    float total =  [[self.billAmountTextField text] floatValue];
+    float totalWithTip = total * (self.currentVal/100) + total ;
+    float divideByPeople = totalWithTip/(1*self.people);
+    NSString* totalWithTipString = [NSString stringWithFormat:@"%.2f", divideByPeople];
+    self.totalLabel.text = totalWithTipString;
+}
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
